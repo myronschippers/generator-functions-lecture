af9b0e9621b5cae708e80f71c84a2b2f3dc4e7fb
Myron <dubtwee@gmail.com>
2019-05-22 09:10:30 -0500

    base code for generator functions lecture

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Generator Functions, What Are they?</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
